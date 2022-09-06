29167e7da02ce4bf421154bfba31ea4d4cea9857
Nataliya <liya15rus@yandex.ru>
2022-09-06 10:08:58 +0300

    Initial commit

---
+++ src/main/java/pro/sky/skyprospringhomework18/SkyproSpringHomework18Application.java
@@ -0,0 +1,13 @@
+package pro.sky.skyprospringhomework18;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproSpringHomework18Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproSpringHomework18Application.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/skyprospringhomework18/SkyproSpringHomework18ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.skyprospringhomework18;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyproSpringHomework18ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
